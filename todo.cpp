12f90f2ecfc753f5061180e1e18addee88eba25a
oscarochoa1 <46940987+oscarochoa1@users.noreply.github.com>
2019-04-20 00:21:59 -0500

    Update todo.cpp

--- todo.cpp
+++ todo.cpp
@@ -5,8 +5,10 @@
 #include <cmath>
 
 using namespace std; 
+using std::cout;
+using std::endl;
 
-void ecuacion(float y0, float wdel, int n, float t, float t1, float t2, float yn, float yn1, float yn2, float yn3, float yn11, float yn21, float yn31);
+void ecuacion(float y0, float wdel, int n, float t, float t1, float t2, float yn, float yn1, float yn2, float yn3, float yn11, float yn21, float yn31, int posicion, int i, int step);
 
 int main(){
     float y0 = 1 ;
@@ -22,13 +24,21 @@ int main(){
     float t = 0.1;
     float t1 = 0.01;
     float t2 = 1;
+    int posicion;
+  int step;
+  int i;
+  srand(time(0));
     
-    ecuacion(y0,wdel,n, t, t1, t2, yn, yn1, yn2, yn3, yn11, yn21, yn31);
+    ecuacion(y0,wdel,n, t, t1, t2, yn, yn1, yn2, yn3, yn11, yn21, yn31, posicion, i, step);
+   
+  
+  
     
     return 0 ; 
 }
 
-void ecuacion(float y0, float wdel, int n,  float t, float t1, float t2, float yn, float yn1,  float yn2, float yn3,float yn11,  float yn21, float yn31){
+
+void ecuacion(float y0, float wdel, int n,  float t, float t1, float t2, float yn, float yn1,  float yn2, float yn3,float yn11,  float yn21, float yn31, int posicion, int i, int step){
     
     yn =  y0 * pow(1 + wdel, -n) ;
     yn1 = yn - t*wdel*yn ;
@@ -37,5 +47,14 @@ void ecuacion(float y0, float wdel, int n,  float t, float t1, float t2, float y
     yn11 = yn - t*wdel*yn11 ;
     yn21 = yn - t1*wdel*yn21 ;
     yn31 = yn - t2*wdel*yn31 ;
-    cout << yn << " " << yn1 << " " << yn2 << " " << yn3 << " " << yn11 << " " << yn21 << " " << yn31 << endl ;
+    
+    i = 0;
+  while(posicion>0){
+    step = rand()%3 -1;
+    posicion = step;
+    cout << i << " "<< posicion ;
+    i++;
+    
+    cout << yn << " Las ecuaciones resueltas son: " << " primera con t=0.1 : " << yn1 << ", segunda con t=0.01 :" << yn2 << ", tercera con t=1 : " << yn3 << ", Seguido sería: " << ", La primera con t=0.1 : "<< yn11 << ", La segunda con t=0.01 : " << yn21 << ", La tercera con t=1 : " << yn31 << ", tiempo es: " << i << endl ;
+}
 }
